ac89832ebca33f584123e2a0571afbdb017b9b17
iency <pwnzjake@gmail.com>
2022-07-22 21:39:37 +0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey Clever Programmer Fam, let's build the Google Clone ❤!</h1>
+    </div>
+  );
+}
+
+export default App;
