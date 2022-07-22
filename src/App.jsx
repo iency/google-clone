5d9cb097867d037c0ea42bb2964f7e5b8e6a728b
iency <pwnzjake@gmail.com>
2022-07-22 21:55:58 +0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
   return (
+    // BEM
     <div className="App">
       <h1>Hey Clever Programmer Fam, let's build the Google Clone ❤!</h1>
+      
+      {/* { Home } */}
+
+      {/* { Search }  */}
     </div>
   );
 }
